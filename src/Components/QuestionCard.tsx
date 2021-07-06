2fefbcb45d326ed8321608fc58af7d1f1d6ea8a6
neharajesh <leehanseok3@gmail.com>
2021-07-06 21:35:35 +0530

    FEAT: Results Page

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -9,13 +9,13 @@ type QuestionCardProps = {
 export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
     const { state, dispatch } = useQuiz();
     const [explanation, setExplanation] = useState<string>("");
+    const [clicked, setClicked] = useState<string>("")
 
     const checkAnswer = (question:QuestionType, option:OptionsType) => {
-        console.log("clicked")
+        setClicked(option._id)
         option.isRight && dispatch({type: "INCREMENT_SCORE"})
         setExplanation(question.explanation)
         const selectedAnswers = [...state.currentQuizAnswers, {currentQuestion: question, selectedAnswerId: option._id}]
-        console.log(selectedAnswers)
         dispatch({type: "ADD_SELECTED_ANSWER", payload: selectedAnswers})
     }
 
@@ -28,7 +28,7 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
             <img className="w-100 mg-b-2" src={question.image} alt={question.questionText} />
             <p className="txt-l mg-025 mg-lr-1"> {question.questionText} </p>
             <p> Points : {question.points} </p>
-            {question.options.map(option => <div className="flex w-75 bdr-thin bdr-rad-m mg-1 pd-1" key={option._id} onClick={() => checkAnswer(question, option)}>
+            {question.options.map(option => <div className={clicked===option._id ? "fill-secondary-yellow flex w-75 bdr-thin bdr-rad-m mg-1 pd-1 csr-point" : "flex w-75 bdr-thin bdr-rad-m mg-1 pd-1 csr-point"} key={option._id} onClick={() => checkAnswer(question, option)}>
                 <p> {option.optionText} </p> 
             </div>)}
             <p className="mg-2 txt-center"> {explanation} </p>
--- src/Pages/ResultPage.tsx
+++ src/Pages/ResultPage.tsx
@@ -1,7 +1,34 @@
+import { useQuiz } from "../Components/Context/Quiz/QuizContext"
+
 export const ResultPage = () => {
+    const { state } = useQuiz();
+
+    const fillOptionDiv = (answer:boolean, questionId:string, optionId:string) => {
+        const selectedAnswer = state.currentQuizAnswers.find(selected => selected.currentQuestion._id === questionId)
+        if (answer)
+            return "fill-secondary-green flex w-75 bdr-thin bdr-rad-m mg-1 pd-1"
+        if (selectedAnswer?.selectedAnswerId === optionId)
+            return "fill-secondary-red flex w-75 bdr-thin bdr-rad-m mg-1 pd-1"
+        return "flex w-75 bdr-thin bdr-rad-m mg-1 pd-1"
+    }
+
     return (<>
-        <div>
+        <div className="w-100 flex-col flex-items-center-y">
             <h1> Result Page </h1>
+            <p> You played : {state.currentQuiz.quizName} </p>
+            <p> You scored : {state.currentScore}/{state.currentQuiz.totalPoints} </p>
+            {state.currentQuiz.questionsList.map(question => <div className="w-50">
+                <div className="w-100 mg-t-1 bdr-thin bdr-rad-m flex-col flex-items-center-y">
+                    <img className="w-100 mg-b-2" src={question.image} alt={question.questionText} />
+                    <p className="txt-l mg-025 mg-lr-1"> {question.questionText} </p>
+                    <p> Points : {question.points} </p>
+                    {question.options.map(option => <div 
+                    className={fillOptionDiv(option.isRight, question._id, option._id)}                        
+                    key={option._id}>
+                        <p> {option.optionText} </p> 
+                    </div>)}
+                </div>
+            </div>)}
         </div>
     </>)
 }
