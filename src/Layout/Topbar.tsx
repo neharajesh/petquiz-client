9e4323d908bbba91db94ba708f63910d19491f1a
neharajesh <leehanseok3@gmail.com>
2021-08-04 21:00:42 +0530

    FIX: Minor Fixes

--- src/Layout/Topbar.tsx
+++ src/Layout/Topbar.tsx
@@ -1,16 +1,16 @@
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom"
 import { useTheme } from "../Components/Context/Theme/ThemeContext";
-import { useUser } from "../Components/Context/User/UserContext"
+// import { useUser } from "../Components/Context/User/UserContext"
 
 export const Topbar = () => {
-    const { state, dispatch } = useUser();
+    // const { state, dispatch } = useUser();
     const { themeState, themeDispatch } = useTheme();
 
-    const logoutHandler = () => {
-        dispatch({type: "USER_LOGOUT"})
-        toast.success("Logged out successfully")
-    }
+    // const logoutHandler = () => {
+    //     dispatch({type: "USER_LOGOUT"})
+    //     toast.success("Logged out successfully")
+    // }
 
     const themeHandler = () => {
         console.log(themeState)
@@ -28,10 +28,10 @@ export const Topbar = () => {
                 <Link className="navLink" to="/"> Home </Link>
                 <Link className="navLink" to="/quiz"> Quiz </Link>
                 
-                {state.user.username === "" 
+                {/* {state.user.username === "" 
                     ? <Link className="navLink" to="/signin"> Login </Link> 
                     : <Link onClick={() => logoutHandler()} className="navLink" to="/signin"> Logout </Link>
-                }
+                } */}
                 
             </nav>
         </div>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export const Home = () => {
     return (<>
         <div>
@@ -5,9 +7,14 @@ export const Home = () => {
             <p className="mg-05"> Welcome to PETQUIZ! </p>
             <p className="mg-05"> This is an animal quiz app, created using React + Typescript </p>
             <br />
-            <h2> What can you do here? </h2>
-            <p className="mg-05"> Play quizzes - with or without signing up </p>
-            <p className="mg-05"> Sign up to view player stats (highscores, quizzes played) and more! Coming Soon! </p>
+            <h2 className="mg-t-05"> What can you do here? </h2>
+            <p className="mg-05"> Play quizzes - without signing up </p>
+            {/* <p className="mg-05"> Sign up to view player stats (highscores, quizzes played) and more! Coming Soon! </p> */}
+            <br />
+            <h2 className="mg-t-05"> Features Coming Soon </h2>
+            <p className="mg-05"> Sign up to view player stats (highscores, quizzes played) and more! </p>
+            <br />
+            <Link to="/quiz" className="playNowButton"> Play Now! </Link>
         </div>
     </>)
 }
